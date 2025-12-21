af6033f0388da850d2c6e5d2c5b799210572c891
qkosyl <qkosyl@gmail.com>
2025-12-21 16:57:20 +0100

    DungeonGenerator complete

---
+++ DungeonGenerator.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+import useful_data
+import json
+
+engine = create_engine("mysql+pymysql://root:kozak123@localhost:3306/rpg_database")
+
+dungeons = useful_data.Dungeons
+
+def assign_data():
+    dungeon_data = []
+    for k, x in dungeons.items():
+        min_level = x[0]
+        max_level = x[1]
+        name = k
+        dungeon_data.append({
+        "name": name,
+        "min_level": min_level,
+        "max_level": max_level
+        })
+    return dungeon_data
+list_of_dungeons = assign_data()
+
+
+with engine.connect() as conn:
+    for dungeon in list_of_dungeons:
+        conn.execute(
+            text("insert into dungeons(name, min_level, max_level)"
+                 "VALUES(:name, :min_level, :max_level)"),
+            dungeon
+        )
+    conn.commit()
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        df = pd.read_sql(text("select * from dungeons"), conn)
+        print(df)
+
---
+++ useful_data.py
@@ -0,0 +1,12 @@
+Dungeons = {
+  "Shadowfang_Crypt": [1,10],
+  "Ashen_Hollow": [11,20],
+  "Obsidian_Depths": [21,30],
+  "Whispering_Catacombs": [31,40],
+  "Sunken_Sanctum": [41,50],
+  "Frostveil_Cavern": [51,60],
+  "Bloodroot_Ruins": [61,70],
+  "Eclipse_Vault": [71,80],
+  "Grimwatch_Tunnels": [81,90],
+  "End_of_All_Realms": [91,100]
+}
