8536d5265bdc0feddb5d937c25191d7c4ef43b98
qkosyl <qkosyl@gmail.com>
2025-12-22 22:34:17 +0100

    PetsGenerator in progress

---
+++ PetsGenerator.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+import useful_data
+import json
+
+#engine = create_engine("mysql+pymysql://root:kozak123@localhost:3306/rpg_database")
+
+pets = useful_data.pets
+
+elements_bonuses = {
+    "Fire": "attack_speed",
+    "Water": "magic_damage",
+    "Ice": "magic_crit_damage",
+    "Plant": "thorns",
+    "Earth": "physical_damage",
+    "Electric": "crit_chance",
+    "Wind": "crit_damage",
+    "Shadow": ["crit_damage","crit_chance"],
+    "light": ["attack_speed","physical_damage"]
+}
+
+def grant_pet_bonus():
+    result = []
+    for pet in pets:
+        if pet["element"] in elements_bonuses.keys():
+            pet["bonus"] = elements_bonuses[pet["element"]]
+            result.append(pet)
+        else:
+            0
+    print(result)
+    return result
+
+grant_pet_bonus()
