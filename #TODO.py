b438345989ed40de9344afabfe2dc3cc07620e26
qkosyl <qkosyl@gmail.com>
2025-12-25 17:07:01 +0100

    TODO LIST

---
+++ #TODO.py
@@ -0,0 +1,45 @@
+'''
+1. Korelacja dungeon <-> monsters
+
+2. Symulacja walki Player < - > (Monster, Dungeon)
+
+3. System Levelowania
+
+4. nadanie itemow potworom
+
+5. losowy drop itemów z monsterów przedział +/- 5 od levela postaci
+
+7. tabela ekwipunek (player_id, bron_id, zbroja_id, rekawice_id, spodnie_id, buty_id, pierscien_id, naszyjnik_id)
+( Ekwipunek_ubrany vs Bagaż )
+8. korelacja itemy <-> postac
+
+9. player musi zawsze mieć na sobie możliwie najlepszy ekwipunek
+
+10. NPC i ich ekwipunek, waluta
+
+11.przeliczanie wartości itemów
+
+12.tradehouse korelacja player wartosc ekwipunek
+
+  3. Trading House ( Player -> Ekwipunek )
+
+-----------------------------------------------------------------------
+
+podział zadan python/database
+
+DataFrames -> Symulacje Walki, Temporary logs of fights and trades,
+QuickCalc -> best equipment, random loot
+
+Database ->
+Static Player Data -> Level, EXP, stats, equipment
+history of items, tradehouses, $$$$
+
+status of World
+Dungeons -> monsters, NPC's
+
+
+
+
+
+
+'''
